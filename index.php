a0a68806a1b80b16636fabd192a27319656aee4f
jrupiter <91055166+jrupiter@users.noreply.github.com>
2021-09-20 14:14:47 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+include("headr.php");
+
+<h1>jrupiter</h1>
+
+
+<input type="text" placeholder="firstname"/> 
+<input type ="text" placeholder="lastname"/>
+<a href="intro1.php"><button>submit</button></a>
+</body>
+
+
+?>
