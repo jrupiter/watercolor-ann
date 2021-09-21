3a873232284fd8cd03c81ffb6284e323ade8f8bf
jrupiter <91055166+jrupiter@users.noreply.github.com>
2021-09-21 19:20:54 -0400

    Create mailthis.php

---
+++ mailthis.php
@@ -0,0 +1,32 @@
+<?php
+$to = $email;
+
+$subject1 = "Jrupiter Moon - Zen";
+$subject2 = "Jrupiter Moon - Self Awareness";
+$subject3 = "Jrupiter Moon - Compatible or Naw";
+
+$msg1 ="Zen
+/r/n
+To know is worth everything. To know yourself is to have control. When the forces of the world try to move you, you will have a stationary position in yourself. However, when the forces of the universe work on you, you will be tapped into what you have to offer so much so that you become an influence. This is in and our colorful description. Find yours.";
+
+$msg2="Self Awareness
+/r/n
+The history of astrology is a great controversial story. Many people do not consider astrology to be a science. Well we don't consider it to be either. A science reports observable facts to describe the objects being observed. 
+/r/n
+What we do is not that. With the help of astrology we help you discover facets of yourself. We hope you groom your consciousness and increase awareness. 
+/r/n
+We use astrology as the painting medium to color you. What picture is more interesting than you?";
+
+$msg3 = "Insightful Matching
+/r/n
+So we know this one sign is compatible with that sign. We know that the alignment of the stars in such a manner speaks about the specific details of how you may find love. 
+/r/n
+We take it one step further. Behind the scenes we paint the picture of you and who you are matching with. Then we hang the pictures up in the gallery together to see if they actually fit in an art show.";
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+$headers .= 'From: <admin@jrupiter.com>' . "\r\n";
+$headers .= 'Cc: tech@jrupiter.com' . "\r\n";
+
+?>
