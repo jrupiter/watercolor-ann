133b5d23a91fd5145c2e02822e51ea1b00531263
jrupiter <91055166+jrupiter@users.noreply.github.com>
2021-09-21 19:11:37 -0400

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+$host = "localhost";
+$user = "U409952495_executor";
+$ps = "U409952495_writ3r!sum";
+$dataB = "u409952495_jrupiter";
+$dbc = mysqli_connect($host, $user, $ps, $dataB) OR die(mysqli_connect_error());
+mysqli_set_charset($dbc, 'utf-8');
+?>
