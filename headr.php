7215053d75205d09c6ab59c71523fbda5e181c5f
jrupiter <91055166+jrupiter@users.noreply.github.com>
2021-09-21 19:43:25 -0400

    Update headr.php

--- headr.php
+++ headr.php
@@ -68,14 +68,88 @@ footer{width:100%; padding:8px; margin:0px;  color:#261c2c;text-align:right;}
 
 section p{padding:10px; margin:0 auto;}
 
-@media(max-width:700px){
-    body{height:950px;}
-    h1{font-size:4em;}
-    form{width:100%;}
-    input{height:50px; padding:10px; width:75%; font-size:1.2em; border:3px;}
-    .button{width:80%;}
-}
+<!Doctype html>
+<html>
+<head>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KGHLPSL3VJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+  gtag('config', 'G-KGHLPSL3VJ');
+  </script>
+    
+<title>jrupiter mood</title>
 
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="icon" href="logo.png" type="image">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400&display=swap" rel="stylesheet"> 
+
+
+ <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet"> 
+
+<style>
+
+
+body{ background-color:ghostwhite; padding:0; margin:0; color: #ffffff; text-align:center;font-size:1em;  }
+
+.land{background-image:url("land.jpg"); background-repeat:no-repeat; background-size:cover;}
+
+h1{color:#261c2c;}
+
+h2,h3{color: #6E85B2;letter-spacing:-2px;}
+
+strong{color:#261c2c;letter-spacing:-2px;}
+
+form{width:80%;padding:10px; margin:0 auto; text-align:center;}
+input{margin:5px; padding:2px; border:1px solid #6E85B2; border-radius:4px;clear:both; font-family:poppins; color:#6e85b2;}
+img{max-width:100%;}
+
+.button{background:#6E85B2; color:#ffffff; border: 1px solid #6e85b2;}    
+
+nav{padding:8px; margin: 0px; width:100%; text-align:left; font-size:2em; }
+
+.intro1{background-image: URL("zen.jpg"); background-repeat: no-repeat; background-size:cover;}
+
+.intro2{background-image: URL("awareness.jpg"); background-repeat: no-repeat; background-size:cover;}
+
+.intro3{background-image: URL("zen.jpg"); background-repeat: no-repeat; background-size:cover;}
+
+header{height:300px; width:100%; padding:8px; margin:0px;}
+
+header p{position: relative; float: left; padding:8px; margin-top:100px; width:30%; color:#261c2c; font-weight:bold; text-align:left;}
+
+section{height:300px; width:100%; padding:8px; margin:0px;  color:#261c2c;}
+
+.alternate{background-color:#f1eff5;}
+.alternate h2{color:#261c2c;}
+
+footer{width:100%; padding:8px; margin:0px;  color:#261c2c;text-align:right;}
+
+section p{padding:10px; margin:0 auto;}
+
+
+@media(max-width:700px){
+body{text-align:center; width:720px; height:920px; background:#5c527f;padding:0px; margin:0px;overflow:none; font-size:1.2em;}
+img{max-width:50%;}
+.loggin{margin-top:10%; text-align:center; padding:0px;width:75%;}
+input{width:35%; margin:6px; padding:5px; border:1px solid #6e85b2; font-size:1.5em; border-radius:4px;}
+.button{background:#6e85b2; padding:10px; margin:6px; width:30%; border:1px solid #6e85b2;font-size:1.5em; border-radius:4px;}
+
+}
+    
+</style>
+</head>
 
     
 </style>
