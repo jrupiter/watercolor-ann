e3783220bd70fa02b88316193fd67bb803c29ad1
jrupiter <91055166+jrupiter@users.noreply.github.com>
2021-09-26 15:14:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <body class="landing">
 <h1>jrupiter</h1>
 
-<form class="loggin" method="post" action="includes/intro1.php">
-<input type="text" placeholder="firstname" value="" name="fname"/> 
+<form class="login" method="post" action="includes/intro1.php">
+<input type="text" placeholder="firstname" value="" name="fname"/> <br>
 <input type="text" placeholder="lastname" value="" name="lname"/> <br>
 <input class="button" type="submit" value="submit" name="submit" /> 
 <form>
