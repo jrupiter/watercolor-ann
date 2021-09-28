44087f383d188d404d3400ac7e8e0701ae2dd0c8
jrupiter <91055166+jrupiter@users.noreply.github.com>
2021-09-28 12:01:08 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,11 +4,14 @@ $user = "u409952495_executor";
 $ps = "writ3r!suM";
 $dataB = "u409952495_jrupiter";
 
-$dbc = mysqli_connect($host, $user, $ps, $dataB);
+$dbc = mysqli_connect($host, $user, $ps, $dataB)
+OR die(mysqli_connect_error());
+mysqli_set_charset($dbc, 'utf-8');
+
 // Check connection
-if (!$dbc) {
-  die("Connection failed: " . mysqli_connect_error());
-}
+//if (!$dbc) {
+//  die("Connection failed: " . //mysqli_connect_error());
+//}
 
 
 ?>
