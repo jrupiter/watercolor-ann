06b1d51ed72057485a84e3f6542ebeb1feee3905
jrupiter <91055166+jrupiter@users.noreply.github.com>
2021-09-28 03:51:57 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,69 @@
-<?php include("includes/headr.php"); ?>
+<?php 
 
-<body class="landing">
+include('includes/headr.php');
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+require('../connect.php');
+$errors = array();
+
+if(empty($_POST['firstname'])){
+$errorrs[] = "Enter your first name.";
+}else{
+$fn = mysqli_real_escape_string($dbc, trim($_POST['firstname']));}
+
+if(empty($_POST['lastname'])){
+$errorrs[] = "Enter your last name.";
+}else{
+$ln = mysqli_real_escape_string($dbc, trim($_POST['lastname']));}
+	
+if(empty($_POST['email'])){
+$errorrs[] = "Enter your email address.";
+}else{
+$e = mysqli_real_escape_string($dbc, trim($_POST['email']));}
+
+if(empty($errors)){
+$q = "SELECT userid FROM users WHERE email='$e'";
+$r = mysqli_query($dbc,$q);
+if(mysqli_num_rows($r) != 0){
+$errors[] = 'Email address already registered. Thank You for Your Support';}
+}
+
+if(empty($errors)){
+$q = "INSERT INTO prelaunch_users (firstname, lastname, email ) VALUES ('$fn', '$ln', '$e', SHA1('$p'), NOW() )";
+$r = mysqli_query($dbc, $q); 
+
+if($r){
+echo'<h1>Registered</h1>
+<p>You are now registered.</p>';
+}
+
+mysqli_close($dbc);
+include('includes/footer.html');
+exit();
+	}else{
+	echo '<h1>Error!</h1>
+	<p id="err_msg">The following error(s) occurred:<br>';
+	foreach($errors as $msg){
+	echo " - $msg<br>";
+	}
+	echo 'Please try again.</p>';
+	mysqli_close($dbc);
+}
+
+}?>
+
+<body class="land">
 <h1>jrupiter</h1>
 
 <form class="login" method="post" action="includes/intro1.php">
-<input type="text" placeholder="firstname" value="" name="fname"/> <br>
-<input type="text" placeholder="lastname" value="" name="lname"/> <br>
-<input type="email" placeholder="email" value="" name="emailaddr"/> <br>
+<input type="text" placeholder="firstname" value="<?php if(isset($_POST['fname'])) echo $_POST['fname']; ?>"> name="fname"/> <br>
+<input type="text" placeholder="lastname" value="<?php if(isset($_POST['lname'])) echo $_POST['lname']; ?>" name="lname"/> <br>
+<input type="email" value="<?php if(isset($_POST['emailaddr'])) echo $_POST['emailaddr'];?>" name="emailaddr"/> <br>
 <input class="button" type="submit" value="submit" name="submit" /> 
+
+<?php include("footer.php"); ?>
 <form>
 </body>
 </html>
+
+
